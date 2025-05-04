a090bbb7dae6fe51a36241de4332ff2160bdde1d
Alper Benzer <118021252+alperbenzer@users.noreply.github.com>
2025-05-04 14:27:25 +0300

    Delete test_scripts directory

--- test_scripts/run_podman.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-podman run -p 8000:8000 \
-  --env-file=.env \
-  -v $(pwd)/aidata.db:/app/aidata.db \
-  transfer_process
-
--- test_scripts/test_pull.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -X GET http://aidata.com.tr/calls \
-  -H "X-API-KEY: slsk6PYL" | jq
-
